3031cd5c80ca6363c841c74fb369d2b65e0bbe96
Liang <392425595@qq.com>
2020-01-09 20:36:14 +0800

    数字被五整除时，函数返回字符串Buzz；重构提取魔法字符串

--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -7,6 +7,9 @@ import static org.junit.Assert.assertEquals;
 public class FizzBuzzTest {
 
     private static final String FIZZ_STRING = "Fizz";
+    private static final String BUZZ_STRING = "Buzz";
+    private static final int FIZZ_NUMBER = 3;
+    private static final int BUZZ_NUMBER = 5;
 
     @Test
     public void should_return_1_when_input_1() {
@@ -18,15 +21,24 @@ public class FizzBuzzTest {
 
     @Test
     public void should_return_Fizz_when_input_3() {
-        String expected = "Fizz";
         String actual = fizzBuzz(3);
 
-        assertEquals(expected, actual);
+        assertEquals(FIZZ_STRING, actual);
+    }
+
+    @Test
+    public void should_return_Buzz_when_input_5() {
+        String actual = fizzBuzz(5);
+
+        assertEquals(BUZZ_STRING, actual);
     }
 
     private String fizzBuzz(int number) {
-        if (number % 3 == 0) {
+        if (number % FIZZ_NUMBER == 0) {
             return FIZZ_STRING;
+        }
+        if (number % BUZZ_NUMBER == 0) {
+            return BUZZ_STRING;
         }
         return String.valueOf(number);
     }
