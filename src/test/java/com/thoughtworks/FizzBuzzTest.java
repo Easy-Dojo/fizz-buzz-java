e26b201e483f3ef5e588e8a8f45c8c229df8e21a
Liang <392425595@qq.com>
2020-01-09 20:29:50 +0800

    数字被三整除时，函数返回字符串Fizz

--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
+
+    private static final String FIZZ_STRING = "Fizz";
+
     @Test
     public void should_return_1_when_input_1() {
         String expected = "1";
@@ -13,9 +16,18 @@ public class FizzBuzzTest {
         assertEquals(expected, actual);
     }
 
-    private String fizzBuzz(int number) {
-        return String.valueOf(number);
+    @Test
+    public void should_return_Fizz_when_input_3() {
+        String expected = "Fizz";
+        String actual = fizzBuzz(3);
+
+        assertEquals(expected, actual);
     }
 
-
+    private String fizzBuzz(int number) {
+        if (number % 3 == 0) {
+            return FIZZ_STRING;
+        }
+        return String.valueOf(number);
+    }
 }
