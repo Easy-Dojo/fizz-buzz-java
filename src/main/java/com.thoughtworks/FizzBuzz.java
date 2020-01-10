3a4bde0e6c247928e91c14f26727853b5dc672cd
Liang <392425595@qq.com>
2020-01-10 20:02:31 +0800

    Rule2：被多个特殊的数字同时整除返回结果

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -7,6 +7,7 @@ public class FizzBuzz {
     private static final int BUZZ_NUMBER = 5;
     private static final String WHIZZ_STRING = "Whizz";
     private static final int WHIZZ_NUMBER = 7;
+    private static final String BLANKSTRING = "";
     private final Integer number;
 
     public FizzBuzz(Integer number) {
@@ -15,17 +16,20 @@ public class FizzBuzz {
 
     @Override
     public String toString() {
-        if (isDivisibleBy(number, FIZZ_NUMBER)) {
-            return FIZZ_STRING;
-        }
-        if (isDivisibleBy(number, BUZZ_NUMBER)) {
-            return BUZZ_STRING;
-        }
-        if (isDivisibleBy(number, WHIZZ_NUMBER)) {
-            return WHIZZ_STRING;
-        }
+        String result = getFizzRuleResult(number) + getBuzzRuleResult(number) + getWhizzRuleResult(number);
+        return result.isEmpty() ? number.toString() : result;
+    }
 
-        return number.toString();
+    private String getFizzRuleResult(Integer number) {
+        return isDivisibleBy(number, FIZZ_NUMBER) ? FIZZ_STRING : BLANKSTRING;
+    }
+
+    private String getBuzzRuleResult(Integer number) {
+        return isDivisibleBy(number, BUZZ_NUMBER) ? BUZZ_STRING : BLANKSTRING;
+    }
+
+    private String getWhizzRuleResult(Integer number) {
+        return isDivisibleBy(number, WHIZZ_NUMBER) ? WHIZZ_STRING : BLANKSTRING;
     }
 
     private boolean isDivisibleBy(Integer number, int i) {
--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -8,12 +8,21 @@ public class FizzBuzzTest {
 
     @ParameterizedTest
     @CsvSource({
-            "1, 1",
-            "Fizz, 3",
-            "Buzz, 5",
-            "Whizz, 7",
+            "FizzBuzz, 15",
     })
     void test_for_input_number_divisible_by_special_number(String expected, Integer number) {
+        FizzBuzz result = new FizzBuzz(number);
+
+        Assertions.assertEquals(expected, result.toString());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "FizzBuzz, 15",
+            "FizzWhizz, 21",
+            "BuzzWhizz, 35",
+    })
+    void test_for_input_number_divisible_by_multi_special_number(String expected, Integer number) {
         FizzBuzz result = new FizzBuzz(number);
 
         Assertions.assertEquals(expected, result.toString());
